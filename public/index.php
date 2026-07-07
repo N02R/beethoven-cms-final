9f98f2209abf74a1687cfb0df1403c2619d1b9ca
N02R <nh9338743@gmail.com>
2026-07-07 22:23:52 +0300

    update index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,15 @@
 <?php
-// استدعاء ملفات الـ Autoloader (أو تضمين الملفات يدوياً إذا لم نستخدم Composer بعد)
+// استدعاء ملفات الـ Core
 require_once '../app/Core/Router.php';
 require_once '../app/Core/Database.php';
+
+// استدعاء ملفات الـ Models
 require_once '../app/Models/User.php';
+
+// استدعاء ملفات الـ Controllers
 require_once '../app/Controllers/AuthController.php';
+require_once '../app/Controllers/DashboardController.php'; // <--- أضيفي هذا السطر
 
-// الحصول على المسار الحالي
 $url = $_SERVER['REQUEST_URI'];
-
-// تشغيل الـ Router
 $router = new \App\Core\Router();
 $router->dispatch($url);

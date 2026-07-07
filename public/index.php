2239bb30634cc075706958ce9b8ad4037a8c0bee
N02R <nh9338743@gmail.com>
2026-07-08 00:04:15 +0300

    update index.php

--- public/index.php
+++ public/index.php
@@ -5,10 +5,11 @@ require_once '../app/Core/Database.php';
 
 // استدعاء ملفات الـ Models
 require_once '../app/Models/User.php';
+require_once '../app/Models/Page.php'; // <--- أضيفي هذا السطر
 
 // استدعاء ملفات الـ Controllers
 require_once '../app/Controllers/AuthController.php';
-require_once '../app/Controllers/DashboardController.php'; // <--- أضيفي هذا السطر
+require_once '../app/Controllers/DashboardController.php';
 
 $url = $_SERVER['REQUEST_URI'];
 $router = new \App\Core\Router();

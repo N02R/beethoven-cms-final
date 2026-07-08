75a3075e6c1d8667728d8b1621868a6c4c565f77
N02R <nh9338743@gmail.com>
2026-07-08 13:16:53 +0300

    update home.php

--- templates/home.php
+++ templates/home.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
-    <?php include __DIR__ . '/layout/head.php'; ?>
+    <?php 
+    echo "";
+    include __DIR__ . '/layout/head.php'; 
+    ?>
     <body>
         <?php 
-        // استدعاء المكونات باستخدام المسار المطلق للمجلد الحالي
+        echo "";
         include __DIR__ . '/layout/header.php';
+        echo "";
         include __DIR__ . '/components/hero.php';
-        include __DIR__ . '/components/services.php';
+        echo "";
         include __DIR__ . '/layout/footer.php';
         ?>
     </body>

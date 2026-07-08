69f2e5974e9dbd45627730abf3e6a05520b23977
N02R <nh9338743@gmail.com>
2026-07-08 16:07:55 +0300

    update

--- app/Core/CMS.php
+++ app/Core/CMS.php
@@ -1,6 +1,5 @@
 <?php
 namespace App\Core;
-
 use PDO;
 
 class CMS {
@@ -10,21 +9,24 @@ class CMS {
             $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
             $stmt = $pdo->prepare("SELECT content FROM site_content WHERE page_key = ? AND section_key = ? AND field_key = ? LIMIT 1");
             $stmt->execute([$page, $section, $field]);
-            return $stmt->fetchColumn() ?: ($field === 'logo_path' ? '/assets/img/logo.png' : '...');
-        } catch (\PDOException $e) { return "Error"; }
+            $res = $stmt->fetchColumn();
+            return $res !== false ? $res : ""; 
+        } catch (\PDOException $e) { return ""; }
     }
 
-    public static function get($page, $section, $field) {
+    public static function get($page, $section, $field, $is_link = false, $is_img = false) {
         $content = self::fetchFromDB($page, $section, $field);
         $isAdmin = (isset($_SESSION['is_admin']) && $_SESSION['is_admin'] === true);
 
-        // إذا كان لوجو
-        if ($field === 'logo_path') {
-            $img = "<img src='$content' alt='Logo' width='178' height='72'>";
-            return $isAdmin ? "<span class='editable-element' data-page='$page' data-section='$section' data-field='$field'>$img<i class='edit-icon'>✏️</i></span>" : $img;
+        if ($is_img) {
+            $element = "<img src='$content' alt='Logo'>";
+        } else {
+            $element = htmlspecialchars($content);
         }
 
-        // إذا كان نص عادي
-        return $isAdmin ? "<span class='editable-element' data-page='$page' data-section='$section' data-field='$field'>" . htmlspecialchars($content) . "<i class='edit-icon'>✏️</i></span>" : htmlspecialchars($content);
+        if ($isAdmin) {
+            return "<span class='editable-element' data-page='$page' data-section='$section' data-field='$field'>$element<i class='edit-icon'>✏️</i></span>";
+        }
+        return $element;
     }
 }
--- templates/layout/header.php
+++ templates/layout/header.php
@@ -1,24 +1,38 @@
 <header>
     <nav class="nav-top navbar py-2">
       <div class="container-fluid custom-container d-flex align-items-center justify-content-between">
-        <a class="navbar-brand d-none d-lg-flex" href="/">
-          <?php echo \App\Core\CMS::get('global', 'header', 'logo_path'); ?>
-        </a>
+        <a class="navbar-brand d-none d-lg-flex" href="/"><?php echo \App\Core\CMS::get('global', 'header', 'logo_path', false, true); ?></a>
         <div class="flex-grow-1 d-none d-lg-flex more"></div>
         <div class="social-icons d-none d-lg-flex gap-3">
-            <a href="<?php echo \App\Core\CMS::get('global', 'header', 'facebook_link'); ?>"><img src="/assets/img/socialicons/Facebook.png"></a>
-            </div>
+          <a href="<?php echo \App\Core\CMS::get('global', 'header', 'facebook_link'); ?>"><img src="assets/img/socialicons/Facebook.png"></a>
+          <a href="<?php echo \App\Core\CMS::get('global', 'header', 'instagram_link'); ?>"><img src="assets/img/socialicons/Instagram.png"></a>
+          <a href="<?php echo \App\Core\CMS::get('global', 'header', 'whatsapp_link'); ?>"><img src="assets/img/socialicons/whatsapp.png"></a>
+          <a href="<?php echo \App\Core\CMS::get('global', 'header', 'twitter_link'); ?>"><img src="assets/img/socialicons/Twitter.png"></a>
+          <a href="<?php echo \App\Core\CMS::get('global', 'header', 'youtube_link'); ?>"><img src="assets/img/socialicons/youtube.png"></a>
+        </div>
       </div>
     </nav>
 
     <nav id="main-header" class="navbar navbar-expand-lg py-3">
       <div class="container-fluid custom-container d-flex align-items-center justify-content-between">
-        <a class="navbar-brand d-lg-none" href="/"><?php echo \App\Core\CMS::get('global', 'header', 'logo_path'); ?></a>
+        <a class="navbar-brand d-lg-none" href="/"><?php echo \App\Core\CMS::get('global', 'header', 'logo_path', false, true); ?></a>
+        
         <div class="collapse navbar-collapse">
           <ul class="navbar-nav gap-3">
-            <li class="nav-item"><a class="nav-link" href="/">الرئيسية</a></li>
-            <li class="nav-item"><a class="nav-link" href="/about">عن الشركة</a></li>
+            <li class="nav-item"><a class="nav-link" href="/"><?php echo \App\Core\CMS::get('global', 'menu', 'home'); ?></a></li>
+            <li class="nav-item"><a class="nav-link" href="/about"><?php echo \App\Core\CMS::get('global', 'menu', 'about'); ?></a></li>
+            <li class="nav-item"><a class="nav-link" href="/education"><?php echo \App\Core\CMS::get('global', 'menu', 'education'); ?></a></li>
+            <li class="nav-item"><a class="nav-link" href="/job"><?php echo \App\Core\CMS::get('global', 'menu', 'job'); ?></a></li>
           </ul>
+        </div>
+        
+        <div class="d-flex align-items-center gap-3">
+          <button class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"><span class="navbar-toggler-icon"></span></button>
+          <div class="dropdown">
+            <button class="btn lang-switch" data-bs-toggle="dropdown">
+                <span><?php echo \App\Core\CMS::get('global', 'header', 'lang_name'); ?></span>
+            </button>
+          </div>
         </div>
       </div>
     </nav>

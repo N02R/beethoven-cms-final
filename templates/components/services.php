7c695d5c4bc9093b9c3b77eca3e951fbb79762c8
N02R <nh9338743@gmail.com>
2026-07-08 14:30:47 +0300

    update services.php

--- templates/components/services.php
+++ templates/components/services.php
@@ -1,30 +1,27 @@
-<!-- templates/components/services.php -->
 <section class="services py-5">
-  <div class="custom-container">
-    <h2 class="mb-5 sec-title"><?php echo \App\Core\CMS::get('home', 'services', 'title'); ?></h2>
-    <div class="row g-4">
-      <!-- الخدمة الأولى -->
-      <div class="col-lg-6 col-md-6 col-sm-12">
-        <div class="card" style="background: url('<?php echo \App\Core\CMS::get('home', 'service1', 'img'); ?>') no-repeat center/cover;">
-          <a href="<?php echo \App\Core\CMS::get('home', 'service1', 'link'); ?>">
-            <div class="card-info">
-              <h3><?php echo \App\Core\CMS::get('home', 'service1', 'title'); ?></h3>
-              <img src="/assets/img/home/Arrow.svg" alt="Arrow">
+    <div class="custom-container">
+      <h2 class="mb-5 sec-title"><?php echo \App\Core\CMS::get('home', 'services', 'main_title'); ?></h2>
+      <div class="row g-4">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <a href="<?php echo \App\Core\CMS::get('home', 'services', 'edu_link'); ?>" class="card-link text-decoration-none">
+            <div class="card" style="background: url('assets/img/home/education.jpg') no-repeat center/cover;">
+              <div class="card-info">
+                <h3><?php echo \App\Core\CMS::get('home', 'services', 'edu_title'); ?></h3>
+                <img src="assets/img/home/Arrow.svg" alt="Arrow">
+              </div>
             </div>
           </a>
         </div>
-      </div>
-      <!-- الخدمة الثانية -->
-      <div class="col-lg-6 col-md-6 col-sm-12">
-        <div class="card" style="background: url('<?php echo \App\Core\CMS::get('home', 'service2', 'img'); ?>') no-repeat center/cover;">
-          <a href="<?php echo \App\Core\CMS::get('home', 'service2', 'link'); ?>">
-            <div class="card-info">
-              <h3><?php echo \App\Core\CMS::get('home', 'service2', 'title'); ?></h3>
-              <img src="/assets/img/home/Arrow.svg" alt="Arrow">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <a href="<?php echo \App\Core\CMS::get('home', 'services', 'job_link'); ?>" class="card-link text-decoration-none">
+            <div class="card" style="background: url('assets/img/home/traning.jpg') no-repeat center/cover;">
+              <div class="card-info">
+                <h3><?php echo \App\Core\CMS::get('home', 'services', 'job_title'); ?></h3>
+                <img src="assets/img/home/Arrow.svg" alt="Arrow">
+              </div>
             </div>
           </a>
         </div>
       </div>
     </div>
-  </div>
 </section>

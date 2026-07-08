3a40d244b4682e2ba6004fd153a584d8b417edee
N02R <nh9338743@gmail.com>
2026-07-08 10:13:47 +0300

    update home.php

--- templates/home.php
+++ templates/home.php
@@ -5,13 +5,13 @@
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
     
-    <?php include 'layout/head.php'; ?>
-    <body>
-    <?php include 'layout/header.php'; ?>
-
+<?php include __DIR__ . '/layout/head.php'; ?>
+<body>
+    <?php include __DIR__ . '/layout/header.php'; ?>
     <main>
         <h1 class="text-center my-5">مرحباً بك في موقع بيتهوفن سيتي</h1>
     </main>
-
-    <?php include 'layout/footer.php'; ?>
-
+    <?php include __DIR__ . '/layout/footer.php'; ?>
+    
+    <script src="/assets/js/bootstrap.bundle.min.js"></script>
+</body>

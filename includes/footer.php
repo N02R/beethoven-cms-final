659ae7eb2e8cb20500b4d760441945cdf2fca7c8
N02R <nh9338743@gmail.com>
2026-07-12 15:37:00 +0300

    update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,10 @@
-  <!-- footer start -->
+<?php
+// تأكيد تهيئة المتغير إذا لم يكن معرفاً في الصفحات الرئيسية
+if (!isset($path_prefix)) {
+    $path_prefix = '';
+}
+?>
+<!-- footer start -->
   <section class="consult-banner-section">
     <div class="container-fluid custom-container">
       <div class="consult-banner">
@@ -7,10 +13,11 @@
             <h4>احصل على استشارة مجانية</h4>
             <p>هذا النص هو مثال لنص يمكن أن يُستبدل في نفس المساحة</p>
           </div>
-          <form class="consult-banner-form" action="send_consult.php" method="POST">
+          <!-- إضافة البادئة لرابط معالجة الفورم -->
+          <form class="consult-banner-form" action="<?php echo $path_prefix; ?>send_consult.php" method="POST">
             <input type="email" name="email" placeholder="ادخل إيميلك..." aria-label="Email" required />
             <button type="submit">
-              <img src="assets/img/home/send-2.svg" alt="إرسال">
+              <img src="<?php echo $path_prefix; ?>assets/img/home/send-2.svg" alt="إرسال">
             </button>
           </form>
         </div>
@@ -22,7 +29,7 @@
     <div class="container-fluid custom-container">
       <div class="row gy-5">
         <div class="col-12 col-lg-5">
-          <img src="assets/img/logo.png" alt="BCS Logo" class="mb-4">
+          <img src="<?php echo $path_prefix; ?>assets/img/logo.png" alt="BCS Logo" class="mb-4">
           <p class="footer-desc">
             هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربي，
             حيث يمكنك..
@@ -31,20 +38,20 @@
         <div class="col-12 col-md-6 col-lg-3">
           <h5>روابط سريعة</h5>
           <div class="quick-link">
-            <a href="about.php">عن الشركة</a>
-            <a href="education.php">التعليم العالي</a>
-            <a href="job.php">التدريب المهني</a>
-            <a href="guide.php">دليل الشركة</a>
-            <a href="contact.php">تواصل معنا</a>
+            <a href="<?php echo $path_prefix; ?>about.php">عن الشركة</a>
+            <a href="<?php echo $path_prefix; ?>education.php">التعليم العالي</a>
+            <a href="<?php echo $path_prefix; ?>job.php">التدريب المهني</a>
+            <a href="<?php echo $path_prefix; ?>guide.php">دليل الشركة</a>
+            <a href="<?php echo $path_prefix; ?>contact.php">تواصل معنا</a>
           </div>
         </div>
         <div class="col-12 col-md-6 col-lg-4">
           <h5>تواصل معنا</h5>
           <div class="contact-link">
-            <a href="tel:+4917671230666"><img src="img/contact%20us/call-calling.svg" alt="">666-230-71 176 (0) 49+</a>
-            <a href="mailto:info@Beethoven-City-Services.com"><img src="img/contact%20us/sms.svg" alt="">info@Beethoven-City-Services.com</a>
+            <a href="tel:+4917671230666"><img src="<?php echo $path_prefix; ?>img/contact%20us/call-calling.svg" alt="">666-230-71 176 (0) 49+</a>
+            <a href="mailto:info@Beethoven-City-Services.com"><img src="<?php echo $path_prefix; ?>img/contact%20us/sms.svg" alt="">info@Beethoven-City-Services.com</a>
             <div class="contact-link-item d-flex align-items-center gap-2">
-              <img src="img/contact%20us/Location.svg" alt="">
+              <img src="<?php echo $path_prefix; ?>img/contact%20us/Location.svg" alt="">
               <span style="font-size: 12px; color: #4F4F4F;">Rheinweg 140 ,53129 Bonn,Germany</span>
             </div>
           </div>
@@ -61,7 +68,7 @@
   </footer>
   <!-- footer end  -->
   
-  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo $path_prefix; ?>assets/js/bootstrap.bundle.min.js"></script>
 
   <!-- حقن ملفات الـ JS الإضافية ديناميكياً قبل باقي الملفات -->
   <?php if (isset($page_js) && is_array($page_js)): ?>
@@ -70,8 +77,8 @@
     <?php endforeach; ?>
   <?php endif; ?>
 
-  <script src="assets/js/all.min.js"></script>
-  <script src="assets/js/main.js"></script>
+  <script src="<?php echo $path_prefix; ?>assets/js/all.min.js"></script>
+  <script src="<?php echo $path_prefix; ?>assets/js/main.js"></script>
 
   <!-- حقن أي سكربت مخصص (Inline Script) تم تعريفه في الصفحة -->
   <?php if (isset($custom_script)) { echo $custom_script; } ?>

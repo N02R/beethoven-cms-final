e175211aa1da30dc1abd11f0312ca4a292a2821d
N02R <nh9338743@gmail.com>
2026-08-02 11:21:32 +0300

    update HomeModel.php

--- src/Models/HomeModel.php
+++ src/Models/HomeModel.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-// أضيفي هذا السطر ليتمكن PHP من العثور على كلاس SiteSettings
 use App\Models\SiteModel;
 
 class HomeModel {
@@ -11,25 +10,28 @@ class HomeModel {
      * جلب وتجهيز بيانات الصفحة الرئيسية فقط
      */
     public static function getHomeData(): array {
-        // جلب كافة الإعدادات باستخدام المودل المركزي SiteSettings
+        // جلب كافة الإعدادات باستخدام المودل المركزي SiteModel
         $settings = SiteModel::getSettings();
         
         // إرجاع مصفوفة منسقة تحتوي على أقسام الصفحة الرئيسية بعد فك تشفير JSON
         return [
-            'hero'                 => isset($settings['hero']) ? json_decode($settings['hero'], true) : [],
+            'hero'                  => isset($settings['hero']) ? json_decode($settings['hero'], true) : [],
+            // سطر جلب وفك تشفير روابط السوشيال ميديا للمودل:
+            'social_links'          => isset($settings['social_links']) ? json_decode($settings['social_links'], true) : [],
+            
             'services_section_title'=> $settings['services_section_title'] ?? '',
             'services_section_desc' => $settings['services_section_desc'] ?? '',
-            'services'             => isset($settings['services']) ? json_decode($settings['services'], true) : [],
-            'choose_title'         => $settings['choose_title'] ?? '',
-            'choose_section_desc'  => $settings['choose_section_desc'] ?? '',
-            'choose_items'         => isset($settings['choose_items']) ? json_decode($settings['choose_items'], true) : [],
-            'reviews_title'        => $settings['reviews_title'] ?? '',
-            'reviews_items'        => isset($settings['reviews_items']) ? json_decode($settings['reviews_items'], true) : [],
-            'guide_title'          => $settings['guide_title'] ?? '',
-            'guide_desc'           => $settings['guide_desc'] ?? '',
-            'guide_items'          => isset($settings['guide_items']) ? json_decode($settings['guide_items'], true) : [],
-            'faq_title'            => $settings['faq_title'] ?? '',
-            'faq_items'            => isset($settings['faq_items']) ? json_decode($settings['faq_items'], true) : [],
+            'services'              => isset($settings['services']) ? json_decode($settings['services'], true) : [],
+            'choose_title'          => $settings['choose_title'] ?? '',
+            'choose_section_desc'   => $settings['choose_section_desc'] ?? '',
+            'choose_items'          => isset($settings['choose_items']) ? json_decode($settings['choose_items'], true) : [],
+            'reviews_title'         => $settings['reviews_title'] ?? '',
+            'reviews_items'         => isset($settings['reviews_items']) ? json_decode($settings['reviews_items'], true) : [],
+            'guide_title'           => $settings['guide_title'] ?? '',
+            'guide_desc'            => $settings['guide_desc'] ?? '',
+            'guide_items'           => isset($settings['guide_items']) ? json_decode($settings['guide_items'], true) : [],
+            'faq_title'             => $settings['faq_title'] ?? '',
+            'faq_items'             => isset($settings['faq_items']) ? json_decode($settings['faq_items'], true) : [],
         ];
     }
 }

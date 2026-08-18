858879712b662f4b56479290a0b1cf45d7c23fd1
N02R <nh9338743@gmail.com>
2026-08-18 18:05:26 +0300

    update cv.php

--- src/Views/edu-services/cv.php
+++ src/Views/edu-services/cv.php
@@ -82,9 +82,12 @@
           </button>
         <?php endif; ?>
 
-        <?php foreach (($cover['download_items'] ?? []) as $item): 
-            $is_pdf = (strtolower($item['type'] ?? '') === 'pdf');
-            $icon = $is_pdf ? 'assets/img/Grouppdf.webp' : 'assets/img/Groupword.webp';
+        <?php 
+          $download_items = $cv_data['download_items'] ?? [];
+          foreach ($download_items as $index => $item):
+              $is_pdf = (strtolower($item['type'] ?? '') === 'pdf');
+              $icon = $is_pdf ? 'assets/img/Grouppdf.webp' : 'assets/img/Groupword.webp';
+              $is_last = ($index === count($download_items) - 1);
         ?>
           <div class="col-lg-12 col-md-12 col-sm-12">
             <div class="download-card <?php echo !$is_last ? 'mb-3' : ''; ?>">
@@ -102,6 +105,7 @@
           </div>
         <?php endforeach; ?>
       </div>
+
 
     </div>
   </section>

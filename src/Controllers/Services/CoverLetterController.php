89f865add33d05d5113f4329cd8fd20a24a12dbc
N02R <nh9338743@gmail.com>
2026-08-24 15:26:19 +0300

    update CoverLetterController.php

--- src/Controllers/Services/CoverLetterController.php
+++ src/Controllers/Services/CoverLetterController.php
@@ -15,7 +15,7 @@ class CoverLetterController {
         if (session_status() === PHP_SESSION_NONE) {
             ini_set('session.cookie_httponly', '1');
             ini_set('session.use_strict_mode', '1');
-            if (isset($_SERVER['HTTPS'] ?? '' ) && $_SERVER['HTTPS'] === 'on') {
+            if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
                 ini_set('session.cookie_secure', '1');
             }
             session_start();

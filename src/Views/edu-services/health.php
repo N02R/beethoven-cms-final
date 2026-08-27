63ac5d92f764dc4b78112380474bddcdf266b45b
N02R <nh9338743@gmail.com>
2026-08-27 12:34:10 +0300

    update health.php

--- src/Views/edu-services/health.php
+++ src/Views/edu-services/health.php
@@ -84,7 +84,7 @@
               <div class="col-lg-4 col-md-6 col-sm-12 mb-2">
                 <li>
                   <p>
-                    <img src="<?php echo get_image_url('assets/img/starList.svg.webp'); ?>" alt="" class="ms-2" />
+                    <img src="<?php echo get_image_url('assets/img/starList.svg.webp'); ?>" alt="" class="ms-2" width="25"/>
                     <?php echo $note; ?>
                   </p>
                 </li>

adba572ab53f6f4a01150592436e251032eb275c
N02R <nh9338743@gmail.com>
2026-09-02 15:45:41 +0300

    update GuideBlog1Controller.php

--- src/Controllers/Guide/GuideBlog1Controller.php
+++ src/Controllers/Guide/GuideBlog1Controller.php
@@ -43,7 +43,8 @@ class GuideBlog1Controller {
 
         // 3. تعريف المتغيرات ومصفوفات الستايل (تمت إزالة style.css لكي لا يلغي ستايل الخدمات)
         $data['path_prefix'] = '/';
-        $data['page_css'] = ['/assets/css/edu-services.css'];
+        $data['page_css'] = ['/assets/css/education.css'];
+
         $data['page_js'] = [];
 
         // تفكيك مصفوفة البيانات لتحويل مفاتيحها إلى متغيرات مستقلة داخل ملفات الـ View
